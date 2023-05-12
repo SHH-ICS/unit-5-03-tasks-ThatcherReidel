7c97d9ca11cf45fddaf920acfa51efbfec3b7f42
Thatcher Reidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-05-12 14:19:46 +0000

    ADDING CODE

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,16 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+question = input("Enter the question: ")
+optionA = input("Enter option A: ")
+optionB = input("Enter option B: ")
+optionC = input("Enter option C: ")
+optionD = input("Enter option D: ")
+correctAnswer = input("Enter the correct answer (A/B/C/D): ")
+
+with open("questions.txt", "a") as file:
+    file.write(question + "\n")
+    file.write("A. " + optionA + "\n")
+    file.write("B. " + optionB + "\n")
+    file.write("C. " + optionC + "\n")
+    file.write("D. " + optionD + "\n")
+    file.write("Answer: " + correctAnswer + "\n\n")
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,24 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+# Open the questions file for reading
+with open("questions.txt", "r") as f:
+    questions = f.readlines()
+
+# Initialize the score
+score = 0
+
+# Loop through each question and ask the user
+for i, question in enumerate(questions):
+    # Split the question and answer using the delimiter
+    question, answer = question.split("::")
+    # Ask the user the question and get their answer
+    user_answer = input(f"Question {i+1}: {question.strip()} ")
+    # Check if the user's answer is correct and update the score if necessary
+    if user_answer.strip().lower() == answer.strip().lower():
+        print("Correct!")
+        score += 1
+    else:
+        print(f"Incorrect. The correct answer is {answer.strip()}")
+
+# Print the final score
+print(f"You got {score} out of {len(questions)} questions correct.")
